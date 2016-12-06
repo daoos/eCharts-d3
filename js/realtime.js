dca64ae3f2fb5cbe3906ad748e78a030ead9287c
Carol1992 <1689597038@qq.com>
2016-12-06 16:14:41 +0800

    gauge

--- js/realtime.js
+++ js/realtime.js
@@ -3,8 +3,8 @@ var height = window.innerHeight;
 var basic = [];
 
 var zoom = d3.behavior.zoom()
-			.scaleExtent([1, 10])
-			.on("zoom", zoomed);
+	.scaleExtent([1, 10])
+	.on("zoom", zoomed);
 
 function zoomed() {
 	d3.select(this).attr("transform", 
@@ -12,7 +12,7 @@ function zoomed() {
 }
 
 var drag = d3.behavior.drag()
-				.on("drag", dragmove); 
+	.on("drag", dragmove); 
 				
 function dragmove(d) {	
 	console.log(d);
@@ -20,23 +20,38 @@ function dragmove(d) {
 }
 
 var svg = d3.select("#charts").append("svg")
-		.attr("width", width)
-		.attr("height", height);
+	.attr("width", width)
+	.attr("height", height);
 
-var groups = svg.append('g')
-		.attr("id","group")
-		.call(zoom)
-		.call(drag);
+var chinaMap = svg.append('g')
+	.attr("id","chinaMap")
+	.call(zoom)
+	.call(drag);
+
+var graphOne = svg.append('g')
+	.attr("class", "display")
+	.attr("id","graphOne")
+	.attr("transform", "translate(" + width*2/3 + "," + 10 +")");
+
+var graphTwo = svg.append('g')
+	.attr("class", "display")
+	.attr("id","graphTwo")
+	.attr("transform", "translate(" + width*2/3 + "," + height*1/3 +")");
+
+var graphThree = svg.append('g')
+	.attr("class", "display")
+	.attr("id","graphThree")
+	.attr("transform", "translate(" + width*2/3 + "," + height*2/3 +")");
 
 var projection = d3.geo.mercator()
-				.center([107,38])
-				.scale(1020)
-				.translate([width/2,height/2]);
+	.center([107,37])
+	.scale(920)
+	.translate([width/3,height/2]);
 
 var path = d3.geo.path()
-				.projection(projection);
+	.projection(projection);
 
-var defs = groups.append('defs');
+var defs = chinaMap.append('defs');
 
 var gaussian = defs.append('filter')
 	.attr('id','gaussian');
@@ -50,11 +65,11 @@ d3.json("json/china.json",function(error, root){
 		return console.error(error);
 	}
 	console.log(root)
+
 	var georoot = root;
 	//var georoot = topojson.feature(root, root.objects.china);
-	//console.log(georoot);
 
-	var paths = groups.selectAll("path")
+	var paths = chinaMap.selectAll("path")
 		.data(georoot.features)
 		.enter()
 		.append('path')
@@ -65,7 +80,7 @@ d3.json("json/china.json",function(error, root){
 		.style("fill", "none")
 		.style("stroke", "#00FFFF");
 
-	var text = groups.selectAll(".place-label")
+	var text = chinaMap.selectAll(".place-label")
 	    .data(georoot.features)
 	  	.enter()
 	  	.append("text")
@@ -77,6 +92,18 @@ d3.json("json/china.json",function(error, root){
 	    .text(function(d) { return d.properties.name; });
 
 	addBasicData();
+	graphOne.append('text')
+		.attr("class", "place-label")
+		.attr("dy", ".35em")
+		.text("Many new syntactic features are included. To natively include and handle multimedia and graphical content, the new <video>, <audio> and <canvas> elements were added, and support for scalable vector graphics (SVG) content and MathML for mathematical formulas. To enrich the semantic content of documents, new page structure elements such as <main>, <section>, <article>, <header>, <footer>, <aside>, <nav> and <figure>, are added. New attributes are introduced, some elements and attributes have been removed, and others such as <a>, <cite> and <menu> have been changed, redefined or standardized.");
+	graphTwo.append('text')
+		.attr("class", "place-label")
+		.attr("dy", ".35em")
+		.text("Many new syntactic features are included. To natively include and handle multimedia and graphical content, the new <video>, <audio> and <canvas> elements were added, and support for scalable vector graphics (SVG) content and MathML for mathematical formulas. To enrich the semantic content of documents, new page structure elements such as <main>, <section>, <article>, <header>, <footer>, <aside>, <nav> and <figure>, are added. New attributes are introduced, some elements and attributes have been removed, and others such as <a>, <cite> and <menu> have been changed, redefined or standardized.");
+	graphThree.append('text')
+		.attr("class", "place-label")
+		.attr("dy", ".35em")
+		.text("Many new syntactic features are included. To natively include and handle multimedia and graphical content, the new <video>, <audio> and <canvas> elements were added, and support for scalable vector graphics (SVG) content and MathML for mathematical formulas. To enrich the semantic content of documents, new page structure elements such as <main>, <section>, <article>, <header>, <footer>, <aside>, <nav> and <figure>, are added. New attributes are introduced, some elements and attributes have been removed, and others such as <a>, <cite> and <menu> have been changed, redefined or standardized.");
 	test();
 })
 
@@ -92,7 +119,7 @@ function addBasicData() {
 		basic = root;
 		console.log(basic);
 
-		var points = svg.selectAll("circle")
+		var points = chinaMap.selectAll("circle")
 			.data(root)
 			.enter()
 			.append('circle')
@@ -101,7 +128,7 @@ function addBasicData() {
 				return "translate(" + projection(d.loc) + ")";
 			})
 			.attr("r",function(d,i){
-				return Math.floor(Math.random() * 1 + 1);
+				return Math.floor(Math.random() * 2 + 1);
 			})
 			.style("fill",function(d,i){
 				//return color(i);
@@ -127,7 +154,7 @@ function test(){
 		var rdm5 = Math.floor(Math.random() * kmsb.length);
 		message = [basic[rdm1].unit_id, status[rdm4], kmwl[rdm3], signal, kmfs[rdm2], kmsb[rdm5]];
 		onMessages(message);
-	}, 600);
+	}, 300);
 }
 
 function onMessages(arr) {
@@ -146,7 +173,7 @@ var i = 0;
 function particle(point) {
 	var m = projection(point);
 	if(m[0] && m[1]) {
-		groups.insert("circle")
+		chinaMap.insert("circle")
 		.attr("cx", m[0])
 		.attr("cy", m[1])
 		.attr("r", 1e-6)
@@ -154,9 +181,9 @@ function particle(point) {
 		.style("stroke", d3.hsl((i = (i + 1) % 360), 1, .5))
 		.style("stroke-opacity", 1)
 	  	.transition()
-	    .duration(3000)
+	    .duration(2000)
 	    .ease(Math.sqrt)
-	    .attr("r", 60)
+	    .attr("r", 85)
 	    .style("fill","none")
 	    .style("stroke-width", '0.1em')
 	    .style("stroke-opacity", 1e-6)
